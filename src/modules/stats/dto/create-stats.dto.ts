295eb48db15d8fb8d44d492336a73214e69727d1
nnesteruk <nikita.nesteruck57@gmail.com>
2026-02-18 17:32:15 +0300

    add description for swagger in stats

--- src/modules/stats/dto/create-stats.dto.ts
+++ src/modules/stats/dto/create-stats.dto.ts
@@ -1,9 +1,108 @@
-export class CreateStatsDto {
-  totalSum: number;
-  totalProducts: number;
+import { ResponseCaseDto } from "@modules/case/dto";
+import { ApiProperty } from "@nestjs/swagger";
+
+type DocumentByCategory = {
+  category: string;
+  count: number;
+};
+
+type ProductByType = {
+  productsType: string;
+  total: number;
+};
+
+export class ResponseStatsDto {
+  @ApiProperty({
+    example: 1000,
+    description: "Общая сумма договоров",
+    type: Number,
+  })
+  totalContractSum: number;
+
+  @ApiProperty({
+    example: [
+      {
+        category: "answer",
+        count: 3,
+      },
+      {
+        category: "contract",
+        count: 5,
+      },
+    ],
+    description: "Общее количество оружия по типу",
+    type: Number,
+  })
+  productsByType: ProductByType[];
+
+  @ApiProperty({
+    example: 17,
+    description: "Общее количество оружия",
+    type: Number,
+  })
+  totalProductsByType: number;
+
+  @ApiProperty({
+    example: 17,
+    description: "Общее количество дел",
+    type: Number,
+  })
   totalCases: number;
-  totalIdentification: number;
-  products: Record<string, number>;
-  documents: { category: string; _sum: { count: number | null } }[];
-  refusesCases;
+
+  @ApiProperty({
+    example: 3,
+    description: "Общее количество идентификаций",
+    type: Number,
+  })
+  totalIdentifications: number;
+
+  @ApiProperty({
+    example: 15,
+    description: "Общее количество документов",
+    type: Number,
+  })
+  totalDocuments: number;
+
+  @ApiProperty({
+    example: [
+      {
+        category: "answer",
+        count: 3,
+      },
+      {
+        category: "contract",
+        count: 5,
+      },
+    ],
+    description: "Общее количество документов по категориям",
+    type: Array,
+  })
+  documents: DocumentByCategory[];
+
+  @ApiProperty({
+    example: [
+      {
+        applicantId: "693de353-5c1b-4e58-b12e-de5e97b8a488",
+        caseNum: 1325,
+        contractDate: null,
+        contractNum: "12351235",
+        contractPaymentDate: null,
+        contractSum: 0,
+        createdAt: "2026-02-09T07:37:25.497Z",
+        id: "7e6f3ae7-8a58-440a-b845-71c8f49b9209",
+        legalForm: "INDIVIDUAL",
+        paymentDeadline: null,
+        paymentNum: "",
+        refusalComment: "Отказано и всё\r\n",
+        registerDate: "2026-02-09T00:00:00.000Z",
+        sertifNum: null,
+        stateApplication: "REFUSED",
+        updatedAt: "2026-02-16T12:35:16.760Z",
+        userId: null,
+      },
+    ],
+    description: "Дела с отказом",
+    type: Array,
+  })
+  refusesCase: ResponseCaseDto[];
 }
--- src/modules/stats/stats.controller.ts
+++ src/modules/stats/stats.controller.ts
@@ -1,8 +1,10 @@
 import { Roles } from "@common/decorators";
+import { ApiCommonResponses } from "@common/decorators/api-common-responses.decorator";
 import { AuthGuard, RolesGuard } from "@common/guards";
 import { Controller, Get, Query, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
 import { Role } from "@prisma/client";
+import { ResponseStatsDto } from "./dto/create-stats.dto";
 import { GetStatsQueryListDto } from "./dto/get-stats-query-list.dto";
 import { StatsService } from "./stats.service";
 
@@ -13,6 +15,15 @@ import { StatsService } from "./stats.service";
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
 
+  @ApiOkResponse({
+    description: "Статистика",
+    type: [ResponseStatsDto],
+  })
+  @ApiOperation({
+    summary: "Получение списка дел",
+    description: "Получает список дел",
+  })
+  @ApiCommonResponses()
   @Get()
   async getStats(@Query() query: GetStatsQueryListDto) {
     return await this.statsService.getStats(query);
