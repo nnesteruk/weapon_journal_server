81256067f008c76c716938f3eba127e664a5a5df
nnesteruk <nikita.nesteruck57@gmail.com>
2026-02-20 15:17:39 +0300

    try fix cookie

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -145,7 +145,7 @@ export class AuthService {
       expires,
       httpOnly: true,
       domain: this.configService.getOrThrow(this.COOKIE_DOMAIN),
-      // secure: !isDevelopment,
+      secure: false,
       sameSite: isDevelopment ? "lax" : "none",
       path: "/",
     });
